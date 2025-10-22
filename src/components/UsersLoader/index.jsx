600323f23ae2c2b374ba9c13b6f9f33328c0426a
Termins55 <kevgame123@gmail.com>
2025-10-22 10:27:43 +0300

    Add UsersLoader base

---
+++ src/components/UsersLoader/index.jsx
@@ -0,0 +1,74 @@
+import { Component } from "react";
+import getUsers from "../api";
+
+class UsersLoader extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      users: [],
+      isFetching: false,
+      error: null,
+      currentPage: 1,
+    };
+  }
+
+  loadUsers = () => {
+    const { currentPage } = this.state;
+
+    this.setState({ isFetching: true });
+    getUsers({
+      page: currentPage,
+      results: 5,
+    })
+      .then((data) => this.setState({ users: data.results }))
+      .catch((e) => this.setState({ error: e }))
+      .finally(() => this.setState({ isFetching: false }));
+  };
+
+  componentDidMount() {
+    this.loadUsers();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { currentPage } = this.state;
+
+    if (currentPage !== prevState.currentPage) {
+      this.loadUsers();
+    }
+  }
+
+  nextPage = () => {
+    const { currentPage } = this.state;
+    this.setState({ currentPage: currentPage + 1 });
+  };
+
+  prevPage = () => {
+    const { currentPage } = this.state;
+    if (currentPage > 1) {
+      this.setState({ currentPage: currentPage - 1 });
+    }
+  };
+
+  render() {
+    const { users, isFetching, error } = this.state;
+    return (
+      <>
+        {error && <div>!!!ERROR!!!{JSON.stringify(error)}</div>}
+        {isFetching && <div>Loading. Please waite...</div>}
+        {!error && !isFetching && (
+          <>
+            <button onClick={this.prevPage}>{"<"}</button>
+            <button onClick={this.nextPage}>{">"}</button>
+            <ul>
+              {users.map((u) => (
+                <li key={u.id}>{JSON.stringify(u)}</li>
+              ))}
+            </ul>
+          </>
+        )}
+      </>
+    );
+  }
+}
+export default UsersLoader;
---
+++ src/components/api/index.js
@@ -0,0 +1,21 @@
+function getUsers(options) {
+  const defaultOptions = {
+    page: 1,
+    results: 10,
+    seed: "pe2022",
+    inc: ["name", "gender", "email", "login"],
+  };
+
+  const realOptions = {
+    ...defaultOptions,
+    ...options,
+  };
+
+  const { page, results, seed, inc } = realOptions;
+
+  return fetch(
+    `https://randomuser.me/api/?page=${page}&results=${results}&seed=${seed}&inc=${inc}`
+  ).then((response) => response.json());
+}
+
+export default getUsers;
